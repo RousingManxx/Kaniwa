e8c5595ef270c5c051b7b0da0ea19b309d052ad9
Marcos Yahir De la caña Pérez <zS20018147@estudiantes.uv.mx>
2022-09-20 08:31:20 -0500

    Primera parte del inicio de google e intento de sesion activa

--- app/src/main/java/com/example/kaniwa/HomeActivity.kt
+++ app/src/main/java/com/example/kaniwa/HomeActivity.kt
@@ -1,11 +1,13 @@
 package com.example.kaniwa
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.kaniwa.databinding.ActivityHomeBinding
 import com.google.firebase.auth.FirebaseAuth
 
 enum class ProviderType{
-    BASIC
+    BASIC,
+    GOOGLE
 }
 
 class HomeActivity : AppCompatActivity() {
@@ -23,12 +25,23 @@ class HomeActivity : AppCompatActivity() {
         val email= bundle?.getString("email")
         val provider = bundle?.getString("provider")
         setup(email ?:"",provider ?:"")
+
+        //gUARDADO DE DATOS
+        val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+        prefs.putString("email",email)
+        prefs.putString("provider",provider)
+        prefs.apply()
     }
     private fun setup(email:String, provider : String){
         title = "Inicio"
         binding.emailTextView.text=email
         binding.providerTextView.text=provider
         binding.closeButton.setOnClickListener{
+            //Borrado de datos de sesion
+            val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+            prefs.clear()
+            prefs.apply()
+
             FirebaseAuth.getInstance().signOut()
             onBackPressed()
         }
